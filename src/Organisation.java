d75faadfd12d527491df012ea51c39072d1e01a1
buxo712 <bwenge45@gmail.com>
2025-03-19 01:32:02 -0700

    commit#10
    in the NV, I generated method to displayed details  and print them in the ORGANISATION class

--- src/Organisation.java
+++ src/Organisation.java
@@ -2,13 +2,15 @@
     class Organisation extends Bankadmnistraction {
         private String matricule1;
 
+
         public Organisation(String admnistractionname, int monthsalary, String matricule1) {
             super(admnistractionname, monthsalary);
-            public void displayDetails() {
-                System.out.println("Admnistractioname: " + getAdmnistractionname() +
-                        ", Monthsalary: " + getMonthsalary() +
-                        ", Matricule1: " + matricule1);
-            }
+            this.matricule1 = matricule1;
         }
+        public void displayDetails() {
+            System.out.println("Admnistractioname: " + getAdmnistractionname() +
+                    ", Monthsalary: " + getMonthsalary() +
+                    ", Matricule1: " + matricule1);
         }
+    }
 
