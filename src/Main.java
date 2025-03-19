edaace0ce02814007a747cafe49b7a84f27bb8c0
buxo712 <bwenge45@gmail.com>
2025-03-18 22:59:47 -0700

    commit#2
    in the NV, i i created instances of the subclasses of the parent ABSTRACTED class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+// Create instances of the subclasses
+        Company comp = new Company("liberty" ,2300,"67");
+        Organisation org = new Organisation("social", 7800, "ugbr4589kjl");
+        // Display details for each object
+        comp.displayDetails();
